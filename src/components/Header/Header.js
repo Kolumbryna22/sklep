9ce1b74a423f19ed7fb8584ce0d519bf5b1ed6c3
Dominik Wilusz <wilusz1@o2.pl>
2019-01-25 17:28:47 +0100

    working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,25 +5,27 @@ import './Header.css';
 export const Header = props => {
   return (
     <div>
-      <nav>
-        <div>
-          Navbar
+      <nav className="navigation">
+        <div className="logo">
+          Logo
         </div>
-        <NavLink
-            exact to="/"
-            activeClassName='activeLink'>
-          Home
-        </NavLink>
-        <NavLink
-            exact to="/user"
-            activeClassName='activeLink'>
-          User
-        </NavLink>
-        <NavLink
-            exact to="/basket"
-            activeClassName='activeLink'>
-          Basket
-        </NavLink>
+        <div className="links">
+          <NavLink
+              exact to="/"
+              activeClassName='activeLink'>
+            Home
+          </NavLink>
+          <NavLink
+              exact to="/user"
+              activeClassName='activeLink'>
+            User
+          </NavLink>
+          <NavLink
+              exact to="/basket"
+              activeClassName='activeLink'>
+            Basket
+          </NavLink>
+        </div>
       </nav>
     </div>
   );
